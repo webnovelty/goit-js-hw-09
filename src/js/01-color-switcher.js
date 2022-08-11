b05281d3d4f65d37a41e5c03e411a623dbf78394
webnovelty <webnoveltystudio@gmail.com>
2022-08-11 22:57:18 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startButtonEl = document.querySelector('[data-start]');
+const stopButtonEl = document.querySelector('[data-stop]');
+let timerId = null;
+stopButtonEl.disabled = true;
 
+startButtonEl.addEventListener('click', startColorChange);
+stopButtonEl.addEventListener('click', stopColorChange);
+function startColorChange() {
+	startButtonEl.disabled = true;
+	stopButtonEl.disabled = false;
+	timerId = setInterval(() => {
+		changeBgColor();
+	}, 1000);
+}
+function changeBgColor() {
+	document.body.style.backgroundColor = getRandomHexColor();
+}
+function stopColorChange() {
+	startButtonEl.disabled = false;
+	stopButtonEl.disabled = true;
+	clearInterval(timerId);
+}
